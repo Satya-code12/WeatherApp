19c20e30b1406c099b8f6d09903d1914dbc1defd
subham5230 <subhammohanty5230@gmail.com>
2024-07-17 21:41:14 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center flex-col">
+        <h1 className="text-[3rem] font-brand">Weather App</h1>
+        <p>Made with ❤️</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        brand: ["Edu AU VIC WA NT Hand", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
