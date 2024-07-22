e72380ae887d52459baa586b14ee38259d215232
Satyabrata Ojha <satyabrat637@gmail.com>
2024-07-22 12:50:44 +0530

    Style:changed

--- src/LandPage2.tsx
+++ src/LandPage2.tsx
@@ -12,19 +12,19 @@ function Content (){
         <div className=" h-9 flex ml-[18.5rem] backdrop-filter backdrop-blur-md bg-opacity-10 bg-gray-500 rounded-md h-[3rem] w-[20rem] border border-gray-300">
           <h1 className="text-white p-[.6rem]" >Filters</h1>
 
-          <select className="m-[.4rem] rounded-sm backdrop-filter backdrop-blur-md bg-opacity-10 bg-gray-500 text-white">
+          <select className="m-[.4rem] rounded-sm backdrop-filter backdrop-blur-md bg-opacity-10 bg-gray-700 text-white">
             <option value="">Coldest</option>
             <option value="opt1">Hot</option>
             <option value="opt2">Spring</option>
           </select>
 
-          <select className="m-[.4rem] rounded-sm backdrop-filter backdrop-blur-md bg-opacity-10 bg-gray-500 text-white">
+          <select className="m-[.4rem] rounded-sm backdrop-filter backdrop-blur-md bg-opacity-10 bg-gray-700 text-white">
             <option value="">Coldest</option>
             <option value="opt1">Hot</option>
             <option value="opt2">Spring</option>
           </select>
 
-          <select className="m-[.4rem] rounded-sm backdrop-filter backdrop-blur-md bg-opacity-10 bg-gray-500 text-white">
+          <select className="m-[.4rem] rounded-sm backdrop-filter backdrop-blur-md bg-opacity-10 bg-gray-700 text-white">
             <option value="">Coldest</option>
             <option value="opt1">Hot</option>
             <option value="opt2">Spring</option>
@@ -35,19 +35,19 @@ function Content (){
      {/* ===============Card Section================= */}
 
     <div className="flex h-[16rem] mt-[3rem]">
-      <div className="backdrop-filter backdrop-blur-md bg-opacity-10 bg-gray-300 border border-gray-300 w-[13rem] ml-[9rem] flex flex-col justify-center items-start pl-3 text-white rounded-lg text-xl">
+      <div className="backdrop-filter backdrop-blur-md bg-opacity-10 bg-gray-600 border border-gray-300 w-[13rem] ml-[9rem] flex flex-col justify-center items-start pl-3 text-white rounded-lg text-xl">
         <h1>Tehran 35</h1>
         <h1>Wind speed: 11km</h1>
         <h1>Tuesday 19:52</h1>
         <h1>Cloudy</h1>
       </div>
-      <div className="backdrop-filter backdrop-blur-md bg-opacity-10 bg-gray-300 border border-gray-300 ml-[7.2rem] w-[13rem] flex flex-col justify-center items-start pl-3 text-white rounded-lg text-xl">
+      <div className="backdrop-filter backdrop-blur-md bg-opacity-10 bg-gray-600 border border-gray-300 ml-[7.2rem] w-[13rem] flex flex-col justify-center items-start pl-3 text-white rounded-lg text-xl">
         <h1>Tehran 35</h1>
         <h1>Wind speed: 11km</h1>
         <h1>Tuesday 19:52</h1>
         <h1>Cloudy</h1>
       </div>
-      <div className="backdrop-filter backdrop-blur-md bg-opacity-10 bg-gray-300 border border-gray-300 ml-[7.2rem] w-[13rem] flex flex-col justify-center items-start pl-3 text-white rounded-lg text-xl">
+      <div className="backdrop-filter backdrop-blur-md bg-opacity-10 bg-gray-600 border border-gray-300 ml-[7.2rem] w-[13rem] flex flex-col justify-center items-start pl-3 text-white rounded-lg text-xl">
         <h1>Tehran 35</h1>
         <h1>Wind speed: 11km</h1>
         <h1>Tuesday 19:52</h1>
--- src/Landpage.tsx
+++ src/Landpage.tsx
@@ -4,12 +4,12 @@ function Navbar () {
   return(
     <>
       <section  className="h-screen">
-      <div className="ml-[9rem] mr-[9rem] py-5 flex text-white">
+      <div className="ml-[8.4rem] mr-[8.4rem] py-5 flex text-white">
         <h2 className="text-2xl font font-semibold">Dark Weather</h2>
-        <h2 className="pl-[9rem] cursor-pointer">Home</h2>
-        <h2 className="pl-9 cursor-pointer">Download App</h2>
-        <h2 className="pl-9 pr-[10rem] cursor-pointer">Contact us</h2>
-        <button className="backdrop-filter backdrop-blur-md bg-opacity-10 w-24 text-base font font-semibold  border rounded-xl text-white border-gray-100">Sign up</button>
+        <h2 className="ml-[9rem] cursor-pointer">Home</h2>
+        <h2 className="ml-9 cursor-pointer">Download App</h2>
+        <h2 className="ml-9 mr-[10rem] cursor-pointer">Contact us</h2>
+        <button className="backdrop-filter backdrop-blur-md bg-opacity-10 w-24 text-base font font-semibold  border rounded-xl text-white border-gray-100 hover:bg-gradient-to-br from-neutral-900 to-teal-600">Sign up</button>
       </div>
 
       <Content/>
