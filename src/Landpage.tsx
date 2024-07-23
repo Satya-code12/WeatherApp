dfd72133a7feb5df0e51f5721922560ae680fa51
Satyabrata Ojha <satyabrat637@gmail.com>
2024-07-23 08:44:54 +0530

    Waste

--- src/Landpage.tsx
+++ src/Landpage.tsx
@@ -4,12 +4,14 @@ function Navbar () {
   return(
     <>
       <section  className="h-screen">
-      <div className="ml-[9.1rem] mr-[8.4rem] py-5 flex text-white">
-        <h2 className="text-2xl font font-semibold">Dark Weather</h2>
-        <h2 className="ml-[9rem] cursor-pointer">Home</h2>
-        <h2 className="ml-9 cursor-pointer">Download App</h2>
-        <h2 className="ml-9 mr-[10rem] cursor-pointer">Contact us</h2>
-        <button className="backdrop-filter backdrop-blur-md bg-opacity-10 w-24 text-base font font-semibold  border rounded-xl text-white border-gray-100 hover:bg-gradient-to-br from-neutral-900 to-teal-600">Sign up</button>
+      <div className="w-[1,277px] h-[78px] mt-[51px] ml-[80px] flex gap-[168px] text-white">
+          <h2 className="w-[281px] h-[60px] text-[40px] text-[40px]">Dark Weather</h2>
+        <div className="w-[495px] h-[38px] flex gap-[50px]">
+          <a href="#">Home</a>
+          <a href="#">Download App</a>
+          <a href="#">Contact us</a>
+        </div>
+        <button className="backdrop-filter backdrop-blur-md bg-opacity-10 w-[165px] h-[78px] text-base font font-semibold  border rounded-[20px] px-[35px] py-[20px] text-white border-gray-100 hover:bg-gradient-to-br from-neutral-900 to-teal-600">Sign up</button>
       </div>
 
       <Content/>
