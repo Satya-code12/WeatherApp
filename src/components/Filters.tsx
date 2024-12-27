a98c8be8c125e74205cd2571600f62cb279b2fc8
Satya-code12 <ojhasatyabrat637@gmail.com>
2024-12-27 09:14:19 +0530

    Style : UI

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -17,7 +17,7 @@ export function Filters() {
     <div className="bg-[#24343D] text-white w-fit h-[40px] rounded-[8px] px-6 p-1 flex items-center gap-x-4">
       <span>Filters</span>
       <select
-        className="bg-black/40 h-full w-fit px-4 py-1 rounded-md border border-white/40"
+        className="bg-black/40 h-full w-fit px-4 py-1 rounded-md border border-white/40 cursor-pointer"
         value={tempratureUnit}
         onChange={(e) => setTempratureUnit(e.target.value as TempratureUnit)}
       >
